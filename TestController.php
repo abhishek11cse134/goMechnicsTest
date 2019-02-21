3146fcf810c2ebccafdf612b593b55f1066902c7
abhishek11cse134 <giet11cse134@gmail.com>
2019-02-21 15:38:50 +0530

    Add files via upload
    3rd problem solution

---
+++ TestController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+       $title=$request->all()['title'];
+       $page=$request->all()['page'];
+       
+       $client = new Client();
+
+       $res = $client->get('https://jsonmock.hackerrank.com/api/movies/search/?Title='.$title.'&page='.$page);
+       $arr=json_decode($res->getBody()->getContents(), true);
+       //dd($arr['data']);
+       $titleArray=[];
+       foreach ($arr['data'] as  $value) {
+        //dd($value);
+          array_push($titleArray,$value['Title']);
+           
+       }
+        //$sortedArray=sort($titleArray);
+       sort($titleArray);
+       $count=count($titleArray);
+       for($x = 0; $x < $count; $x++) {
+        echo $titleArray[$x];
+         echo "<br>";
+        }
+        //dd($sortedArray);
+
+    
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
